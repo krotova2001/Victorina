4fe93ae1061ac230221dfc6aee5496a91cda3306
George Krotov <zvuk24@gmail.com>
2022-10-23 21:33:54 +0700

    Добавьте файлы проекта.

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victorina_exam_
+{
+    //класс пользователя
+    internal class User
+    {
+        string login;
+        string password;
+        public DateTime birthday;
+        bool is_admin = false;
+        int score;
+        public User (string log, string pass)
+        {
+            login = log;
+            password = pass;
+        }
+        public string Login { get { return login; } }
+        public bool Is_admin { get { return is_admin; } }
+        public int Score { get { return score; } }
+    }
+}
---
+++ Vict.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victorina_exam_
+{
+    //класс викторины
+    class Vict
+    {
+        string[] questions; // вопросы
+        Dictionary<string, int> answers; //варианты и ответов и баллы. индексы совпадают с вопросами
+        string theme; // тема или раздел викторины
+    }
+}
