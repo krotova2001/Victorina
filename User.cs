5e6b88055b5ad041bc79130ec86219d049af9656
George Krotov <zvuk24@gmail.com>
2022-10-27 16:52:46 +0700

    12

--- User.cs
+++ User.cs
@@ -9,8 +9,8 @@ namespace Victorina_exam_
     //класс пользователя
     class User
     {
-        string login; // логин
-        string password; // пароль
+        public string Login { get; set; } // логин
+        public string Password { get; set; } // пароль
         public DateTime birthday { get; set; }
         bool is_admin = false; // по умолчанию юзер - не администратор
         int score=0; //очки от пройденных викторин
@@ -18,14 +18,12 @@ namespace Victorina_exam_
 
         public User(string login, string password, DateTime birthday, bool is_admin)
         {
-            this.login = login;
-            this.password = password;
+            this.Login = login;
+            this.Password = password;
             this.birthday = birthday;
             this.is_admin = is_admin;
         }
 
-        public string Login { get { return login; } } // показать логин
-        public string Password { get { return password; } } // показать пароль
         public bool Is_admin { get { return is_admin; } } // показать права администратора
         public int Score { get { return score; } } // показать очки (ретинг)
         public void Inc_score (int s) { score += s; } // доабвить очкт после прохождения теста
